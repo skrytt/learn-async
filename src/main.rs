437430e6c46a3978c765f01a8ef6c79a6b5c052e
Skrytt <matt.kemp.1992@gmail.com>
2024-03-03 21:36:37 +0000

    tokio in 2nd thread, print stuff

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,66 @@
 mod user_option;
 
+use ansi_term::{
+  Colour::{Green, Red, RGB},
+  Style,
+};
 use inquire::{
     InquireError, Select
 };
+use std::sync::mpsc::channel;
+use std::thread;
+use std::time::Duration;
+use tokio::time::sleep;
 use user_option::UserOption;
 
-fn get_user_decision() -> Result<UserOption, InquireError> {
+fn get_test_type() -> Result<UserOption, InquireError> {
     let options = vec![
-        UserOption::new("Follow the road".to_string()),
-        UserOption::new("Take a nap".to_string()),
+        UserOption::new("HTTP".to_string()),
+        UserOption::new("TCP".to_string()),
+        UserOption::new("DNS".to_string()),
+        UserOption::new("CloudFormation".to_string()),
     ];
 
-    let result = Select::new("What would you like to do?", options).prompt()?;
-
+    let result = Select::new("Select Test Mode:", options).prompt()?;
     Ok(result)
 }
 
+// TODO: a way to spawn this in a separate thread to TUI
+fn tokio_runtime() {
+
+  thread::spawn(|| {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(async {
+      let mut i = 0;
+      let sleep_millis = 1000;
+      loop {
+        sleep(Duration::from_millis(sleep_millis)).await;
+        i += sleep_millis;
+        println!("Slept {} millis", i);
+      }
+    })
+  });
+}
+
 fn main() {
-  get_user_decision().unwrap();
+  match get_test_type() {
+    Err(e) => println!("{}", e),
+    _ => (),
+  }
+
+  const DARKGREY: ansi_term::Colour = RGB(60, 60, 60);
+
+  println!("{}{} {}", 
+    Style::new().on(DARKGREY).paint(" 14:45:54 "),
+    Style::new().bold().on(Green).paint(" 200 ↻ "),
+    Style::new().bold().paint("https://www.rust-lang.org")
+  );
+  println!("{}{} {}", 
+    Style::new().on(DARKGREY).paint(" 14:45:59 "),
+    Style::new().bold().on(Red).paint(" TIMEOUT [5s] "),
+    Style::new().bold().paint("https://www.rust-lang.org")
+  );
+
+  tokio_runtime();
+  std::thread::sleep(Duration::from_millis(10000));
 }
