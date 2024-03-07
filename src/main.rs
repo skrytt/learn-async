e2d0b2b6b9626913a362bf8f6e7e7f3104314be4
Skrytt <matt.kemp.1992@gmail.com>
2024-03-07 17:58:44 +0000

    ratatui example counter app error handing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod errors;
 mod http;
 mod reactor;
 mod tui;
@@ -5,18 +6,26 @@ mod user_input;
 
 use std::io;
 
+use color_eyre::{
+  eyre::{bail, WrapErr},
+  Result,
+};
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
-    prelude::*,
-    symbols::border,
-    widgets::{block::*, *},
+  prelude::*,
+  symbols::border,
+  widgets::{
+      block::{Position, Title},
+      *,
+  },
 };
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
+  errors::install_hooks()?;
   let mut terminal = tui::init()?;
-  let app_result = App::default().run(&mut terminal);
+  App::default().run(&mut terminal)?;
   tui::restore()?;
-  app_result
+  Ok(())
 }
 
 #[derive(Debug, Default)]
@@ -26,84 +35,92 @@ pub struct App {
 }
 
 impl App {
-  /// runs the application's main loop until the user quits
-  pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
-      while !self.exit {
-          terminal.draw(|frame| self.render_frame(frame))?;
-          self.handle_events()?;
-      }
-      Ok(())
-  }
-
-  fn render_frame(&self, frame: &mut Frame) {
-    frame.render_widget(self, frame.size());
-  }
-
-  fn handle_events(&mut self) -> io::Result<()> {
-    match event::read()? {
-        // it's important to check that the event is a key press event as
-        // crossterm also emits key release and repeat events on Windows.
-        Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-            self.handle_key_event(key_event)
+    /// runs the application's main loop until the user quits
+    pub fn run(&mut self, terminal: &mut tui::Tui) -> Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.render_frame(frame))?;
+            self.handle_events().wrap_err("handle events failed")?;
         }
-        _ => {}
-    };
-    Ok(())
-  }
-
-  fn exit(&mut self) {
-    self.exit = true;
-  }
-
-  fn increment_counter(&mut self) {
-      self.counter += 1;
-  }
-
-  fn decrement_counter(&mut self) {
-      self.counter -= 1;
-  }
-
-  fn handle_key_event(&mut self, key_event: KeyEvent) {
-    match key_event.code {
-        KeyCode::Char('q') => self.exit(),
-        KeyCode::Left => self.decrement_counter(),
-        KeyCode::Right => self.increment_counter(),
-        _ => {}
+        Ok(())
     }
-  }
+
+    fn render_frame(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.size());
+    }
+
+    /// updates the application's state based on user input
+    fn handle_events(&mut self) -> Result<()> {
+        match event::read()? {
+            // it's important to check that the event is a key press event as
+            // crossterm also emits key release and repeat events on Windows.
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event).wrap_err_with(|| {
+                    format!("handling key event failed:\n{key_event:#?}")
+                })
+            }
+            _ => Ok(()),
+        }
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) -> Result<()> {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left => self.decrement_counter()?,
+            KeyCode::Right => self.increment_counter()?,
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn decrement_counter(&mut self) -> Result<()> {
+        self.counter -= 1;
+        Ok(())
+    }
+
+    fn increment_counter(&mut self) -> Result<()> {
+        self.counter += 1;
+        if self.counter > 2 {
+            bail!("counter overflow");
+        }
+        Ok(())
+    }
 }
 
 impl Widget for &App {
-  fn render(self, area: Rect, buf: &mut Buffer) {
-      let title = Title::from(" Counter App Tutorial ".bold());
-      let instructions = Title::from(Line::from(vec![
-          " Decrement ".into(),
-          "<Left>".blue().bold(),
-          " Increment ".into(),
-          "<Right>".blue().bold(),
-          " Quit ".into(),
-          "<Q> ".blue().bold(),
-      ]));
-      let block = Block::default()
-          .title(title.alignment(Alignment::Center))
-          .title(
-              instructions
-                  .alignment(Alignment::Center)
-                  .position(Position::Bottom),
-          )
-          .borders(Borders::ALL)
-          .border_set(border::THICK);
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Title::from(" Counter App Tutorial ".bold());
+        let instructions = Title::from(Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]));
+        let block = Block::default()
+            .title(title.alignment(Alignment::Center))
+            .title(
+                instructions
+                    .alignment(Alignment::Center)
+                    .position(Position::Bottom),
+            )
+            .borders(Borders::ALL)
+            .border_set(border::THICK);
 
-      let counter_text = Text::from(vec![Line::from(vec![
-          "Value: ".into(),
-          self.counter.to_string().yellow(),
-      ])]);
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
 
-      Paragraph::new(counter_text)
-          .centered()
-          .block(block)
-          .render(area, buf);
-  }
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
 }
 
 // ================
